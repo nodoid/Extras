3464662283c707667a48960ca2bcec83808d2ec0
nodoid <paul@snuberry-software.co.uk>
2015-02-16 00:32:01 +0000

    Initial import

---
+++ Extras/Extras/CalledActivity.cs
@@ -0,0 +1,53 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+
+namespace Extras
+{
+    [Activity(Label = "CalledActivity")]			
+    public class CalledActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.CalledActivity);
+            GetParameters();
+
+            var nextActivity = FindViewById<Button>(Resource.Id.button1);
+            nextActivity.Click += delegate
+            {
+                var bndle = new Bundle();
+                var firstName = FindViewById<EditText>(Resource.Id.firstName2);
+                var lastName = FindViewById<EditText>(Resource.Id.lastName2);
+                var age = FindViewById<EditText>(Resource.Id.age2);
+
+                bndle.PutString("firstName", firstName.Text);
+                bndle.PutString("lastName", lastName.Text);
+                bndle.PutInt("age", int.Parse(age.Text));
+
+                var intent = new Intent();
+                intent.PutExtras(bndle);
+                SetResult(Result.Ok, intent);
+                Finish();
+            };
+        }
+
+        void GetParameters()
+        {
+            if (this.Intent.Extras != null)
+            {
+                var bundle = this.Intent.Extras;
+
+                var firstName = FindViewById<EditText>(Resource.Id.firstName2);
+                var lastName = FindViewById<EditText>(Resource.Id.lastName2);
+                var age = FindViewById<EditText>(Resource.Id.age2);
+
+                firstName.Text = bundle.GetString("firstName");
+                lastName.Text = bundle.GetString("lastName");
+                age.Text = bundle.GetInt("age").ToString();
+            }
+        }
+    }
+}
+
---
+++ Extras/Extras/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Widget;
+using Android.OS;
+
+namespace Extras
+{
+    [Activity(Label = "Extras", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        const int CALLED_ACTIVITY = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.CallingActivity);
+
+            var nextActivity = FindViewById<Button>(Resource.Id.button1);
+            nextActivity.Click += delegate
+            {
+                var intent = new Intent(this, typeof(CalledActivity));
+                var bndle = new Bundle();
+                var firstName = FindViewById<EditText>(Resource.Id.firstName);
+                var lastName = FindViewById<EditText>(Resource.Id.lastName);
+                var age = FindViewById<EditText>(Resource.Id.age);
+
+                bndle.PutString("firstName", firstName.Text);
+                bndle.PutString("lastName", lastName.Text);
+                bndle.PutInt("age", int.Parse(age.Text));
+
+                intent.PutExtras(bndle);
+                StartActivityForResult(intent, CALLED_ACTIVITY);
+            };
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            switch (requestCode)
+            {
+                case CALLED_ACTIVITY:
+                    if (resultCode == Result.Ok)
+                    {
+                        var bundle = data.Extras;
+                        var firstName = FindViewById<EditText>(Resource.Id.firstName);
+                        var lastName = FindViewById<EditText>(Resource.Id.lastName);
+                        var age = FindViewById<EditText>(Resource.Id.age);
+                        firstName.Text = bundle.GetString("firstName");
+                        lastName.Text = bundle.GetString("lastName");
+                        age.Text = bundle.GetInt("age").ToString();
+                    }
+                    break;
+            }
+        }
+    }
+}
+
+
